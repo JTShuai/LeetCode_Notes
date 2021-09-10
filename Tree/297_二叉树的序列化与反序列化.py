fc0bcd704d460a224d63f8418e020f3c8d3cdcb3
shuai_Macbook <shuaijt1110@gmail.com>
2021-09-11 01:25:25 +0200

    add: leetcode297_二叉树的序列化与反序列化

--- Tree/297_二叉树的序列化与反序列化.py
+++ Tree/297_二叉树的序列化与反序列化.py
@@ -1,7 +1,50 @@
 '''
 
 '''
+from Tree.TreeNode import TreeNode
 
-if __name__ == '__main__':
-    a = '1,2,12,13'
-    b = a.split(',')
+
+def serialize(self, root):
+    """Encodes a tree to a single string.
+
+    :type root: TreeNode
+    :rtype: str
+    """
+    # 前序遍历
+    def dfs(root):
+        if not root:
+            return 'null'
+
+        root_val = root.val
+
+        left = dfs(root.left)
+        right = dfs(root.right)
+
+        return str(root_val)+','+left + ',' + right
+
+    return dfs(root)
+
+
+def deserialize(self, data):
+    """Decodes your encoded data to tree.
+
+    :type data: str
+    :rtype: TreeNode
+    """
+    if not data: return None
+
+    data = data.split(',')
+    root = self.buildTree(data)
+    return root
+
+
+def buildTree(self, data):
+    val = data.pop(0)
+    if val == 'null': return None
+
+    node = TreeNode(val)
+
+    node.left = self.buildTree(data)
+    node.right = self.buildTree(data)
+
+    return node
